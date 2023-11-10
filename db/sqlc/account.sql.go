02278e15b7d776b6ede9733b338aaa7480f1a580
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2023-11-09 22:59:12 -0300

    sqlc generated go code

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -10,13 +10,9 @@ import (
 )
 
 const createAccount = `-- name: CreateAccount :one
-INSERT INTO accounts (
-  owner, 
-  balance, 
-  currency
-) VALUES (
-  $1, $2, $3
-) RETURNING id, owner, balance, currency, created_at
+INSERT INTO accounts (owner, balance, currency)
+VALUES ($1, $2, $3)
+RETURNING id, owner, balance, currency, created_at
 `
 
 type CreateAccountParams struct {
@@ -49,8 +45,10 @@ func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
 }
 
 const getAccount = `-- name: GetAccount :one
-SELECT id, owner, balance, currency, created_at FROM accounts
-WHERE id = $1 LIMIT 1
+SELECT id, owner, balance, currency, created_at
+FROM accounts
+WHERE id = $1
+LIMIT 1
 `
 
 func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
@@ -67,10 +65,10 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
 }
 
 const listAccounts = `-- name: ListAccounts :many
-SELECT id, owner, balance, currency, created_at FROM accounts
+SELECT id, owner, balance, currency, created_at
+FROM accounts
 ORDER BY id
-LIMIT $1
-OFFSET $2
+LIMIT $1 OFFSET $2
 `
 
 type ListAccountsParams struct {
@@ -108,7 +106,7 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 }
 
 const updateAccount = `-- name: UpdateAccount :one
-UPDATE accounts 
+UPDATE accounts
 SET balance = $2
 WHERE id = $1
 RETURNING id, owner, balance, currency, created_at
